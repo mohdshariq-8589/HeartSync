58ce7fc3ef096691d869c875c70644c354a775a9
Mohd Shariq <155702117+mohdsha-123@users.noreply.github.com>
2025-04-26 00:04:14 +0530

    Add React Parellex to the app

--- client/src/components/SwipeArea.jsx
+++ client/src/components/SwipeArea.jsx
@@ -1,4 +1,5 @@
 import TinderCard from "react-tinder-card";
+import Tilt from "react-parallax-tilt";
 import { useMatchStore } from "../store/useMatchStore";
 
 const SwipeArea = () => {
@@ -20,24 +21,26 @@ const SwipeArea = () => {
           swipeThreshold={100}
           preventSwipe={["up", "down"]}
         >
-          <div
-            className="card bg-white w-96 h-[28rem] select-none rounded-lg overflow-hidden border
-					 border-gray-200"
-          >
-            <figure className="px-4 pt-4 h-3/4">
-              <img
-                src={user.image || "/avatar.png"}
-                alt={user.name}
-                className="rounded-lg object-cover h-full pointer-events-none"
-              />
-            </figure>
-            <div className="card-body bg-gradient-to-b from-white to-pink-50">
-              <h2 className="card-title text-2xl text-gray-800">
-                {user.name}, {user.age}
-              </h2>
-              <p className="text-gray-600">{user.bio}</p>
+          <Tilt glareEnable={true} glareMaxOpacity={0.2} scale={1.05}>
+            <div
+              className="card bg-white w-96 h-[28rem] select-none rounded-lg overflow-hidden border
+						 border-gray-200"
+            >
+              <figure className="px-4 pt-4 h-3/4">
+                <img
+                  src={user.image || "/avatar.png"}
+                  alt={user.name}
+                  className="rounded-lg object-cover h-full pointer-events-none"
+                />
+              </figure>
+              <div className="card-body bg-gradient-to-b from-white to-pink-50">
+                <h2 className="card-title text-2xl text-gray-800">
+                  {user.name}, {user.age}
+                </h2>
+                <p className="text-gray-600">{user.bio}</p>
+              </div>
             </div>
-          </div>
+          </Tilt>
         </TinderCard>
       ))}
     </div>
